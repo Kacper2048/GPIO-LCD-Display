5471d4ddd064273ff76bea595e5bcf64efd4899e
Spion-Spion <105750231+Spion-Spion@users.noreply.github.com>
2022-11-28 16:48:05 +0100

    0.1.6

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@ void LCD_DATA(int);
 void LCD_CMD(unsigned char CMD);
 void LCD_String(string str);
 void LCD_xxx(int val);
+void LCD_Clear();
+void LCD_Set_Cursor(unsigned char r, unsigned char c);
+
 
 #define LCD_EN_Delay 500
 
@@ -33,7 +36,12 @@ int main(int argc, char *argv[])
             init_8pins(fd,rq);
 
             LCD_Init();
+            LCD_Clear();
+            LCD_Set_Cursor(1,1);
             LCD_String("hej");
+            
+            cout << "ok chyba powinno być ok" << endl;
+            usleep(5000000);
             close_file(fd);
         }
         
@@ -42,53 +50,72 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+void LCD_Set_Cursor(unsigned char r, unsigned char c)
+{
+  unsigned char Temp,Low4,High4;
+  if(r == 1)
+  {
+    Temp = 0x80 + c - 1; //0x80 is used to move the cursor
+    High4 = Temp >> 4;
+    Low4 = Temp & 0x0F;
+    LCD_CMD(High4);
+    LCD_CMD(Low4);
+  }
+
+  if(r == 2)
+  {
+    Temp = 0xC0 + c - 1;
+    High4 = Temp >> 4;
+    Low4 = Temp & 0x0F;
+    LCD_CMD(High4);
+    LCD_CMD(Low4);
+  }
+}
+
+void LCD_Write_Char(char Data)
+{
+  char Low4,High4;
+  Low4 = Data & 0x0F;
+  High4 = Data & 0xF0;
+
+  LCD_xxx(static_cast<int>(High4>>4));
+  LCD_xxx(static_cast<int>(Low4));
+}
+
+void LCD_Clear()
+{
+    LCD_CMD(0);
+    LCD_CMD(1);
+}
+
 void LCD_String(string str)
 {
     for(int i=0;i<str.length(); i++)
     {
-        LCD_xxx(static_cast<int>(str[i]));
+        LCD_Write_Char(str[i]);
     }
 }
 
 void LCD_xxx(int val) //The main goal of that function is to change int value to binary form 
 {
     // Select Data Register
-    //RS = 0;
+    //RS = 1;
     tab[5] = 1;
 
-    int upper = val & 0xF0;
-    int lower = val & 0x0F;
-
-    //sending upper part of data to lcd
     for(int i=0;i<4;i++)
     {
-        (upper & static_cast<int>(pow(2,i)))? tab[i] = 1 : tab[i] = 0;
+        (val & static_cast<int>(pow(2,i)))? tab[i] = 1 : tab[i] = 0;
     }
+   
     // Send The EN Clock Signal
     //EN = 1;
     tab[4] = 1;
-
-    usleep(LCD_EN_Delay);
     send_8bit(fd,rq,tab); 
     usleep(LCD_EN_Delay);
+
     tab[4] = 0;
     send_8bit(fd,rq,tab);
-    
-
-    //sending lower part of data to lcd
-    for(int i=0;i<4;i++)
-    {
-        (lower & static_cast<int>(pow(2,i)))? tab[i] = 1 : tab[i] = 0;
-    }
-    // Send The EN Clock Signal
-    //EN = 1;
-    tab[4] = 1;
-
     usleep(LCD_EN_Delay);
-    send_8bit(fd,rq,tab); 
-
-    tab[4] = 0;
-    send_8bit(fd,rq,tab); 
 
 }
 
@@ -135,7 +162,8 @@ void LCD_CMD(unsigned char CMD) //4bit mode
 
 void LCD_Init()
 {
-  // The Init. Procedure
+  // The Init. Procedure //1st option
+  
   LCD_DATA(0x00);
   usleep(30);
   usleep(LCD_EN_Delay);
@@ -151,4 +179,5 @@ void LCD_Init()
   LCD_CMD(0x0C);
   LCD_CMD(0x00);
   LCD_CMD(0x06);
+
 }
